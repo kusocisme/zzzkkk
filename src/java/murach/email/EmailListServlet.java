fff6ea0ca410b76f954926feb60a4852b7d32341
cusocisme@gmail.com <cusocisme@gmail.com>
2023-02-23 15:16:53 +0700

    first commit

---
+++ src/java/murach/email/EmailListServlet.java
@@ -0,0 +1,121 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+
+package murach.email;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import murach.business.User;
+import murach.data.UserDB;
+
+
+/**
+ *
+ * @author ADMIN
+ */
+@WebServlet(name="EmailListServlet", urlPatterns={"/email"})
+public class EmailListServlet extends HttpServlet {
+   
+    /** 
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet EmailListServlet</title>");  
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet EmailListServlet at " + request.getContextPath () + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    } 
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /** 
+     * Handles the HTTP <code>GET</code> method.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+        protected void doGet(HttpServletRequest request, 
+                         HttpServletResponse response) 
+                         throws ServletException, IOException {
+        doPost(request, response);
+
+    } 
+
+    /** 
+     * Handles the HTTP <code>POST</code> method.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, 
+                          HttpServletResponse response) 
+                          throws ServletException, IOException {
+
+        String url = "/index.html";
+
+        // get current action
+        String action = request.getParameter("action");
+        if (action == null) {
+            action = "join";  // default action
+        }
+                // perform action and set URL to appropriate page
+        if (action.equals("join")) {
+            url = "/index.html";    // the "join" page
+        }
+        else if (action.equals("add")) {                
+            // get parameters from the request
+            String firstName = request.getParameter("firstName");
+            String lastName = request.getParameter("lastName");
+            String email = request.getParameter("email");
+
+            // store data in User object and save User object in db
+            User user = new User(firstName, lastName, email);
+            //UserDB.insert(user);
+            
+            // set User object in request object and set URL
+            request.setAttribute("user", user);
+            url = "/thanks.jsp";   // the "thanks" page
+        }
+        
+        // forward request and response objects to specified URL
+        getServletContext()
+            .getRequestDispatcher(url)
+            .forward(request, response);
+    }    
+
+    }
+
+    /** 
+     * Returns a short description of the servlet.
+     * @return a String containing servlet description
+     */
+    //@Override
+    //public String getServletInfo() {
+       // return "Short description";
+    //}// </editor-fold>
+//}
+
